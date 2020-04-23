cc55eb4a3920605a76924f2f4c676eca408b3d96
Wallace <devwallace93@gmail.com>
2020-04-23 15:44:09 -0300

    Version 1.0 (Dev)

--- agendexFront/src/pages/Profile/index.js
+++ agendexFront/src/pages/Profile/index.js
@@ -1,9 +1,37 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux'
+import { Form, Input} from '@rocketseat/unform'
 
-// import { Container } from './styles';
+import { updateProfileRequest} from '~/store/modules/user/actions'
+
+import { Container } from './styled';
 
 export default function Profile() {
-  return (
-    <div />
+  const dispatch = useDispatch()
+  const profile = useSelector(state => state.user.profile)
+
+  function handleSubmit(data) {
+    dispatch(updateProfileRequest(data))
+  }
+
+  return ( 
+  <Container>
+    <Form initialData={profile} onSubmit={handleSubmit}>
+      <Input name="name" placeholder="Nome Completo" />
+      <Input name="email" type="email" placeholder="Seu endereço de e-mail" />
+
+      <hr/>
+
+      <Input type="password" name="password" placeholder="Sua senha atual" /> 
+      <Input type="password" name="password" placeholder="Sua nova Senha" /> 
+      <Input type="password" name="password" placeholder="Confirme a nova Senha" />   
+
+      <button type="submit">Atualizar</button> 
+    </Form>
+
+    <button type="button">
+      Sair do MarcaJogo
+    </button>
+  </Container>
   );
 }
